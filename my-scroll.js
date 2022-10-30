b55fa8ad14acb1fdfb86a4b630bf1c49c6c3cd88
dungdanghung <dungprof8.0@gmail.com>
2022-10-30 11:30:51 +0700

    first commit

---
+++ my-scroll.js
@@ -0,0 +1,81 @@
+import "./my-scroll.css"
+import { useEffect, useState , useRef } from "react"
+
+
+function Scroll({ children, setbackgrou }) {
+    const [scroll, setscroll] = useState('0')
+    const [scrollitem, setscrollitem] = useState()
+    const [all, setall] = useState();
+    const ref = useRef();
+
+    function handlescroll(e) {
+        e.target.scrollTop > 20 ? setbackgrou("#1e1e1e") : setbackgrou("transparent")
+        setscroll(all.scrollheight / 100 * ((e.target.scrollTop + e.target.offsetHeight) / all.heightconten) * 100 - (parseInt(scrollitem.slice(0, scrollitem.length - 1)) / 100 * all.scrollheight))
+    }
+
+
+    function handlemove(e) {
+        if (e.pageY - handlemove.prototype.ofsetyclick + document.querySelector('.scrollitem').offsetHeight > all.scrollheight - 1 || e.pageY - handlemove.prototype.ofsetyclick < 0) {
+            return;
+        }
+        setscroll(e.pageY - handlemove.prototype.ofsetyclick)
+        all.wrapcontens.scrollTop = (((e.pageY - handlemove.prototype.ofsetyclick + document.querySelector('.scrollitem').offsetHeight) / all.scrollheight) * 100) * all.heightconten / 100 - all.wrapcontens.offsetHeight
+
+    }
+
+    function handlescrolldow(e) {
+        if (e.target.className == 'scroll') {
+            if (e.nativeEvent.offsetY + document.querySelector('.scrollitem').offsetHeight / 2 >= e.target.offsetHeight) {
+                setscroll(e.target.offsetHeight - document.querySelector('.scrollitem').offsetHeight)
+                all.wrapcontens.scrollTop = all.wrapcontens.children[0].offsetHeight - all.wrapcontens.offsetHeight
+            } else if (e.nativeEvent.offsetY - document.querySelector('.scrollitem').offsetHeight / 2 <= 0) {
+                setscroll(0)
+                all.wrapcontens.scrollTop = 0
+            }
+            else {
+                setscroll(e.nativeEvent.offsetY - document.querySelector('.scrollitem').offsetHeight / 2)
+                all.wrapcontens.scrollTop = ((e.nativeEvent.offsetY + document.querySelector('.scrollitem').offsetHeight / 2) / all.scrollheight * 100) * all.heightconten / 100 - all.wrapcontens.offsetHeight
+            }
+        }
+
+        else if (e.target.className == 'scrollitem') {
+            handlemove.prototype.ofsetyclick = e.nativeEvent.offsetY
+            window.onmousemove = handlemove
+            window.onmouseup = () => {
+                window.onmousemove = null
+            }
+        }
+    }
+
+    ref.current = new ResizeObserver((a)=>{
+        setall({
+            scrollheight: document.querySelector('.scroll').offsetHeight,
+            heightconten: document.querySelector('.wrapconten').children[0].children[0].offsetHeight,
+            wrapcontens: document.querySelector('.wrapcontens')
+        })
+        setscrollitem((document.querySelector('.scroll').offsetHeight / document.querySelector('.wrapconten').children[0].children[0].offsetHeight * 100).toFixed(0) + '%')
+    })
+
+    useEffect(() => {
+        ref.current.observe(document.querySelector('.wrapcontens'))
+        ref.current.observe(document.querySelector('.wrapconten').children[0].children[0])
+    }, [])
+
+    
+    return (
+        <>
+            <div className="wrapconten">
+                <div className="wrapcontens" onScroll={handlescroll}>
+                    {children}
+                </div>
+                <div className='scroll' style={{ display: 'block' }} onMouseDown={handlescrolldow}>
+                    <div className='scrollitem' style={{ top: scroll ? scroll : 0, height: scrollitem }} ></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+
+export default Scroll
